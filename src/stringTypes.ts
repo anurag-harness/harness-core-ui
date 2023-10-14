5cbec8946ff4ff286403e54292c38a6ad0bc49fd
Anurag Madnawat <anurag.madnawat@harness.io>
2023-10-14 02:16:52 +0000

    feat: [CET-1193] Add Monitored Services list to CET

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -176,6 +176,7 @@ export interface StringsMap {
   'common.cluster': string
   'common.clusterName': string
   'common.clusters': string
+  'common.codeErrorsMonitoredServices': string
   'common.codeErrorsSettings': string
   'common.collapse': string
   'common.combined': string
