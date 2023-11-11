05d5b47b5b30e95c17b08dd8abbc6a49531ac001
Anurag Madnawat <anurag.madnawat@harness.io>
2023-11-10 18:30:26 -0800

    fix: [OIP-752]: Make health sources API call when user navigates to logs screen directly

--- src/modules/85-cv/components/ExecutionVerification/tests/ExecutionVerificationView.test.tsx
+++ src/modules/85-cv/components/ExecutionVerification/tests/ExecutionVerificationView.test.tsx
@@ -10,12 +10,19 @@ import userEvent from '@testing-library/user-event'
 import { render, waitFor, screen } from '@testing-library/react'
 import { findDialogContainer, TestWrapper } from '@common/utils/testUtils'
 import type { ExecutionNode } from 'services/pipeline-ng'
+import * as cvService from 'services/cv'
+import * as commonHooks from '@common/hooks'
 import {
   LogTypes,
   SLOLogContentProps,
   VerifyStepLogContentProps
 } from '@cv/hooks/useLogContentHook/useLogContentHook.types'
 import { ExecutionVerificationView } from '../ExecutionVerificationView'
+import {
+  HealthSourcesResponse,
+  expectedHealthSourcesParams,
+  expectedHealthSourcesParamsWithMetrics
+} from './ExecutionVerificationView.mock'
 import { getActivityId, getDefaultTabId } from '../ExecutionVerificationView.utils'
 
 jest.mock('../components/DeploymentMetrics/DeploymentMetrics', () => ({
@@ -168,5 +175,55 @@ describe('Unit tests for ExecutionVerificationView unit tests', () => {
     })
 
     await userEvent.click(dialog?.querySelector('[data-icon="Stroke"]')!)
+  })
+
+  test('should make health sources API call if user opens logs tabs directly', async () => {
+    jest.spyOn(commonHooks, 'useQueryParams').mockImplementation(() => ({ type: 'Logs' }))
+
+    const useGetHealthSourcesForVerifyStepExecutionIdSpy = jest
+      .spyOn(cvService, 'useGetHealthSourcesForVerifyStepExecutionId')
+      .mockReturnValue({
+        data: HealthSourcesResponse,
+        refetch: jest.fn() as unknown
+      } as any)
+
+    render(
+      <TestWrapper
+        path="/:accountId/:orgIdentifier/:projectIdentifier"
+        pathParams={{ accountId: 'acc', orgIdentifier: 'org', projectIdentifier: 'project' }}
+      >
+        <ExecutionVerificationView step={{ progressData: { activityId: '1234_activityId' as any } }} />
+      </TestWrapper>
+    )
+
+    await waitFor(() =>
+      expect(useGetHealthSourcesForVerifyStepExecutionIdSpy).toHaveBeenCalledWith(expectedHealthSourcesParams)
+    )
+  })
+
+  test('should call health sources API call as lazy if user opens Metrics tabs directly', async () => {
+    jest.spyOn(commonHooks, 'useQueryParams').mockImplementation(() => ({ type: 'Metrics' }))
+
+    const useGetHealthSourcesForVerifyStepExecutionIdSpy = jest
+      .spyOn(cvService, 'useGetHealthSourcesForVerifyStepExecutionId')
+      .mockReturnValue({
+        data: HealthSourcesResponse,
+        refetch: jest.fn() as unknown
+      } as any)
+
+    render(
+      <TestWrapper
+        path="/:accountId/:orgIdentifier/:projectIdentifier"
+        pathParams={{ accountId: 'acc', orgIdentifier: 'org', projectIdentifier: 'project' }}
+      >
+        <ExecutionVerificationView step={{ progressData: { activityId: '1234_activityId' as any } }} />
+      </TestWrapper>
+    )
+
+    await waitFor(() =>
+      expect(useGetHealthSourcesForVerifyStepExecutionIdSpy).toHaveBeenCalledWith(
+        expectedHealthSourcesParamsWithMetrics
+      )
+    )
   })
 })
